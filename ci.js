15b526d340b003c3cef38b166acdbde916e13e1d
Hiroki Osame <hiroki.osame@gmail.com>
2021-04-11 18:44:21 -0400

    feat: detect which pnpm version to use closes #3

--- ci.js
+++ ci.js
@@ -9,12 +9,24 @@ var opts = {
 	shell: true,
 };
 
+var parseNodeVersion = process.version.match(/^v(\d+)\.(\d+)\.(\d+)/);
+var nodeVersion = parseNodeVersion.slice(1, 4).map(v => parseInt(v, 10));
+function compareSemver(semverA, semverB) {
+	return (
+		semverA[0] - semverB[0]
+		|| semverA[1] - semverB[1]
+		|| semverA[2] - semverB[2]
+	);
+}
+
 if (existsSync('package-lock.json')) {
 	spawnSync('npm', ['ci'], opts);
 } else if (existsSync('yarn.lock')) {
 	spawnSync('npx', ['yarn', 'install', '--immutable'], opts);
 } else if (existsSync('pnpm-lock.yaml')) {
-	spawnSync('npx', ['pnpm', 'i', '--frozen-lockfile'], opts);
+	// https://github.com/pnpm/pnpm/releases/tag/v6.0.0-rc.1
+	var pnpmVersion = compareSemver(nodeVersion, [12, 17, 0]) >= 0 ? 'pnpm@6' : 'pnpm@5';
+	spawnSync('npx', [pnpmVersion, 'i', '--frozen-lockfile'], opts);
 } else {
 	console.error('Error: No lock file (package-lock.json, yarn.lock, pnpm-lock.yaml) found');
 	process.exit(1);
